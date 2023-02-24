a26440bc68513f73f77c64b1ace67c35f3dea3cd
Julio Salgado <jyahirsalgado@gmail.com>
2023-02-24 16:43:07 -0600

    Creacion de variables necesarias del inciso b

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,12 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+void asignaciones_constantes_fechas();
+void generacion_idOrden();
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -46,4 +53,34 @@ int MainWindow::connectPSQL()
 {
 
     return 0;
+}
+
+void asignaciones_constantes_fechas(){
+    time_t tp = time(nullptr);
+    tm* fo = localtime(&tp);
+    string fecha_orden, fecha_requerida, fecha_enviada = NULL;  //Fechas requeridas del inciso b
+
+    int dia = fo->tm_mday;
+    int mes = fo->tm_mon + 1;
+    int anno = fo->tm_year + 1900;
+
+    fecha_orden = to_string(dia) + "/" + to_string(mes) + "/" + to_string(anno);
+
+    tp += 172800;
+    fo = localtime(&tp);
+    mes = fo->tm_mon + 1;
+    dia = fo->tm_mday;
+    anno = fo->tm_year + 1900;
+
+    fecha_requerida = to_string(dia) + "/" + to_string(mes) + "/" + to_string(anno);
+
+    cout << fecha_orden << endl;
+    cout << fecha_requerida;
+}
+
+void generacion_idOrden(){
+    srand(time(NULL));
+
+    int id_orden = 10000 + rand() % (100000 - 10000);       //Generacion Aleatoria del OrderID
+    cout << id_orden << endl;
 }
