f3ea8957a3cfa09b3e4d4dc017f6848522a5e13b
jairoaguilarts <jairoaguilar1219@gmail.com>
2023-03-03 13:52:45 -0600

    Ajusta al tamaño de la tabla los datos

--- reportes.cpp
+++ reportes.cpp
@@ -39,16 +39,16 @@ void Reportes::on_pushButton_clicked()
     QString id = uiReporte->LE_ID->text();
     QSqlQueryModel *queryModel = new QSqlQueryModel();
     if(cliente){
-        queryModel->setQuery(QString("SELECT extract(year from orders.order_date) AS Year, "
-                                     "extract(month from orders.order_date) AS Month, "
-                                     "SUM(order_details.quantity * order_details.unit_price) AS TotalPaid "
+        queryModel->setQuery(QString("SELECT extract(year from orders.order_date) AS Año, "
+                                     "extract(month from orders.order_date) AS Mes, "
+                                     "SUM(order_details.quantity * order_details.unit_price) AS Total "
                                      "FROM customers JOIN orders ON customers.customer_id = orders.customer_id "
                                      "JOIN order_details ON orders.order_id = order_details.order_id "
                                      "WHERE customers.customer_id = '%1' "
                                      "GROUP BY extract(year from orders.order_date), extract(month from orders.order_date) "
                                      "ORDER BY extract(year from orders.order_date), extract(month from orders.order_date)").arg(id));
     } else if(proveedor){
-        queryModel->setQuery(QString("SELECT EXTRACT(YEAR FROM o.order_date) AS Year, SUM(od.quantity) AS TotalSold "
+        queryModel->setQuery(QString("SELECT EXTRACT(YEAR FROM o.order_date) AS Año, SUM(od.quantity) AS Productos_Vendidos "
                              "FROM suppliers s "
                              "JOIN products p ON s.supplier_id = p.supplier_id "
                              "JOIN order_details od ON p.product_id = od.product_id "
@@ -57,7 +57,7 @@ void Reportes::on_pushButton_clicked()
                              "GROUP BY EXTRACT(YEAR FROM o.order_date) "
                              "ORDER BY EXTRACT(YEAR FROM o.order_date)").arg(id.toInt()));
     } else if(categoria){
-        queryModel->setQuery(QString("SELECT c.category_name, COUNT(p.product_id) AS total_products, SUM(od.quantity) AS total_sold "
+        queryModel->setQuery(QString("SELECT c.category_name Nombre_Categoria, COUNT(p.product_id) AS Productos_Totales, SUM(od.quantity) AS Productos_Vendidos "
                                      "FROM categories c "
                                      "JOIN products p ON c.category_id = p.category_id "
                                      "JOIN order_details od ON p.product_id = od.product_id "
@@ -66,4 +66,6 @@ void Reportes::on_pushButton_clicked()
                                      "ORDER BY c.category_name").arg(id.toInt()));
     }
     uiReporte->TV_Reporte->setModel(queryModel);
+    uiReporte->TV_Reporte->resizeColumnsToContents();
+    uiReporte->TV_Reporte->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
 }
